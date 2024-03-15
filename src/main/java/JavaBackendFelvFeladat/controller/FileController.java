16d732107644d4a39146ba224dfab296c2e5dc61
ambrus.viktor <ambrus.viktor94@gmail.com>
2024-03-15 18:13:17 +0100

    pom.xml dependecies, springboot setup

---
+++ src/main/java/JavaBackendFelvFeladat/controller/FileController.java
@@ -0,0 +1,14 @@
+package JavaBackendFelvFeladat.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/files")
+@AllArgsConstructor
+@Slf4j
+public class FileController {
+
+}
